2062c5ba9f9558c1e7fae62aad6fe91722c63798
Thomas Nyambati <12892110+nyambati@users.noreply.github.com>
2024-04-09 14:41:17 +0200

    add sudo sso url

--- root/esaiga/functions.sh
+++ root/esaiga/functions.sh
@@ -1,26 +1,5 @@
 WORKSPACE=$HOME/workspace/
 
-profiles=(
-    pandacare-stg
-    pandacare-prod
-    contact-center-prd
-    contact-center-stg
-    log-dwh-prd
-    log-dwh-stg
-    log-prd
-    log-prd-backup
-    log-shared
-    log-stg
-    log-vendor-prd
-    log-vendor-prd-backup
-    log-vendor-stg
-    pickers-prd
-)
-
-
-alias sso='for profile in ${profiles[@]}; do saml2aws --skip-prompt login --force -a $profile; done;'
-
-
 hf() {
     readonly labels=$1
     readonly command=$2
@@ -36,4 +15,8 @@ ste() {
     for secret in ${SECRETS[@]}; do
         export $secret
     done
+}
+
+sso() {
+    open $SUDO_AWS_URL
 }
