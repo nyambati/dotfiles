8ca91acef623594a4637eb47919361a7d0b5862f
Thomas Nyambati <12892110+nyambati@users.noreply.github.com>
2026-02-13 15:20:29 +0100

    update helmfile functions
    * changes
    
    * ...

--- root/esaiga/functions.sh
+++ root/esaiga/functions.sh
@@ -1,10 +1,18 @@
 WORKSPACE=$HOME/workspace/
 
+
 hf() {
     readonly labels=$1
     readonly command=$2
-    [ -z "$labels" ] && echo "Missing chart labels key=value"; return
-    [ -z "$command" ] && echo "missing command diff|sync"; return
+    if [ -z "$labels" ]; then
+        echo "Missing chart labels key=value"
+        return
+    fi
+    if [ -z "$command" ]; then
+        echo "missing command diff|sync"
+        return
+    fi
+
     helmfile --file $WORKSPACE/hero/logistics-kubernetes/helmfiles/observability/helmfile.yaml --skip-deps -l $labels $command
 }
 
@@ -19,4 +27,16 @@ ste() {
 
 sso() {
     open $SUDO_AWS_URL
+}
+
+hfd(){
+    hf $@ diff
+}
+
+hfs(){
+    hf $@ sync
+}
+
+hfe(){
+    hf $@ destroy
 }
